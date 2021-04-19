8d9b936d6178a3ea603ffeede9afa2992504eb3b
Iqbal Septyan <iqbalseptyan@gmail.com>
2021-04-19 16:15:52 +0700

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './components/Navbar';
+import Particles from "react-particles-js";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import { Fragment } from "react";
 function App() {
   return (
-    <Navbar/>
+    <Fragment>
+      <Particles
+        params={{
+          particles: {
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#F7DC6F",
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+    </Fragment>
   );
 }
 
